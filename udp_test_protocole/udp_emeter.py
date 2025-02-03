0c58fe76f5978aa143e3b124e603d46dacb39bdd
JungMatthieu <114075428+JungMatthieu@users.noreply.github.com>
2025-02-03 15:25:24 +0100

    test UDP

--- udp_test_protocole/udp_emeter.py
+++ udp_test_protocole/udp_emeter.py
@@ -1,8 +1,11 @@
 import socket
+import threading
 
 # Adresse IP de l'antenne réceptrice et le port
-RECEIVER_IP = "192.168.254.102"  # Remplacez par l'IP de l'antenne réceptrice
+#RECEIVER_IP = "127.0.0.1"
+RECEIVER_IP = "192.168.254.115"   # Remplacez par l'IP de l'antenne réceptrice
 RECEIVER_PORT = 14555
+ADDR = (RECEIVER_IP,RECEIVER_PORT)
 
 # Création du socket UDP
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -10,8 +13,8 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 print("Émetteur UDP prêt à envoyer des messages.")
 try:
     while True:
-        message = input("Entrez un message à envoyer : ")
-        sock.sendto(message.encode(), (RECEIVER_IP, RECEIVER_PORT))
+        message = "test"
+        sock.sendto(message.encode('utf_8'), ADDR)
         print("Message envoyé !")
 
 except KeyboardInterrupt:
--- udp_test_protocole/udp_recepteur.py
+++ udp_test_protocole/udp_recepteur.py
@@ -1,7 +1,10 @@
 import socket
+import json
 
 # Adresse IP d'écoute et le port
-LISTEN_IP = "0.0.0.0"  # Écoute sur toutes les interfaces réseau
+LISTEN_IP = "0.0.0.0"  # L'ordinateur récepteur écoute sur toutes les interfaces réseau
+#LISTEN_IP = "192.168.254.115"  # L'ordinateur récepteur écoute sur toutes les interfaces réseau
+#LISTEN_IP = "127.0.0.1"
 LISTEN_PORT = 14555
 
 # Création du socket UDP
@@ -10,12 +13,8 @@ sock.bind((LISTEN_IP, LISTEN_PORT))
 
 print(f"Récepteur UDP en attente de messages sur {LISTEN_IP}:{LISTEN_PORT}...")
 
-try:
-    while True:
-        data, addr = sock.recvfrom(1024)  # Réception des données (max 1024 octets)
-        print(f"Message reçu de {addr}: {data.decode()}")
-
-except KeyboardInterrupt:
-    print("\nArrêt du récepteur.")
-finally:
-    sock.close()
+while True:
+    data0, addr = sock.recvfrom(1024)  # Réception des données (max 1024 octets)
+    print("data received : ", data0)
+    data = data0.decode('utf-8')
+    print(f"Message reçu de {addr}: {data}")
